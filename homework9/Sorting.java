c55495424c92555909d85cbe0a1eb5b9cb393068
Anna Chukreeva <ann-chukreeva@mail.ru>
2022-05-26 22:31:18 +0300

    Homework_9

--- homework9/Sorting.java
+++ homework9/Sorting.java
@@ -1,6 +1,7 @@
 package homework.homework9;
 
 import java.util.Arrays;
+import java.util.Random;
 
 public class Sorting {
     public static void sorting(int[] array){
@@ -21,12 +22,18 @@ public class Sorting {
         }
     }
     public static void main(String[] args) {
-        int[] array = {5,8,10,1,1,20};
+        int[] array = new int [1000000];
+        Random random = new Random();
+             for(int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt();}
+        long starTime1 = System.currentTimeMillis();
         sorting(array);
-        for(int i = 0; i < array.length; i++) {
-            System.out.printf("%d ",array[i]);
-        }
-
+        long endTime1 = System.currentTimeMillis();
+        System.out.println("Сортирвка вставками: " + (endTime1 - starTime1));
+        long starTime2 = System.currentTimeMillis();
+        Arrays.sort(array);
+        long endTime2 = System.currentTimeMillis();
+        System.out.println("Встроенный алгоритм: " + (endTime2 - starTime2));
     }
     }
 
